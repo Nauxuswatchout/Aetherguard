f30f5577b3bd2f8f79ac7ebfa6431b4ea5987f3a
Nauxuswatchout <nauxuswatchout@gmail.com>
2025-05-02 05:41:55 +1000

    Delete static/css directory

--- static/css/js/guidelines.js
+++
@@ -1,8 +0,0 @@
-function toggleCollapse(tipId) {
-    const tipBox = document.getElementById(tipId);
-    if (tipBox.style.display === 'none' || tipBox.style.display === '') {
-        tipBox.style.display = 'block';
-    } else {
-        tipBox.style.display = 'none';
-    }
-}
